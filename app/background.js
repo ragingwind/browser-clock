2a7e1a683ca8319c7f18e92189787c4b42b67de1
ragingwind <ragingwind@gmail.com>
2016-06-23 05:47:40 +0900

    Using universal loader

--- app/background.js
+++ app/background.js
@@ -1,35 +1,3 @@
-class Colour {
-  constructor() {
-    this.color = [0, 0, 0, 255];
-  }
-
-  change() {
-    return this.color;
-  }
-}
-
-class HueCycleColour extends Colour {
-  constructor() {
-    super();
-
-    this.frequency = 3;
-    this.iterator = 0;
-  }
-
-  change() {
-    this.color[0] = Math.floor(Math.sin(this.frequency * this.iterator * 0) * 127 + 128);
-    this.color[2] = Math.floor(Math.sin(this.frequency * this.iterator * 4) * 127 + 128);
-    this.color[1] = Math.floor(Math.sin(this.frequency * this.iterator * 2) * 127 + 128);
-    this.iterator++;
-
-    if (this.iterator >= 32) {
-      this.iterator = 0;
-    }
-
-    return this.color;
-  }
-}
-
 class Clock {
 	constructor() {
 		this.color = new HueCycleColour();
---
+++ app/colour.js
@@ -0,0 +1,41 @@
+// Base class for Colour
+class Colour {
+  constructor() {
+    this.color = [0, 0, 0, 255];
+  }
+
+  change() {
+    return this.color;
+  }
+}
+
+// HueCycleColour pattern drawer
+class HueCycleColour extends Colour {
+  constructor() {
+    super();
+
+    this.frequency = 3;
+    this.iterator = 0;
+  }
+
+  change() {
+    this.color[0] = Math.floor(Math.sin(this.frequency * this.iterator * 0) * 127 + 128);
+    this.color[2] = Math.floor(Math.sin(this.frequency * this.iterator * 4) * 127 + 128);
+    this.color[1] = Math.floor(Math.sin(this.frequency * this.iterator * 2) * 127 + 128);
+    this.iterator++;
+
+    if (this.iterator >= 32) {
+      this.iterator = 0;
+    }
+
+    return this.color;
+  }
+}
+
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global || {})));
+}(this, function (exports) { 'use strict';
+  exports.Colour = Colour;
+}));
